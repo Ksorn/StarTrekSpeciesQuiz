0e2dc6b27a7a841fa23cf7f62b760d526740732d
Kristijan Šorn <kristijan.sorn@gmail.com>
2017-02-09 16:31:26 +0100

    Update #1

--- app/src/main/java/com/example/android/startrekspeciesquiz/MainActivity.java
+++ app/src/main/java/com/example/android/startrekspeciesquiz/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.android.startrekspeciesquiz;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean answeredAll;
-    boolean editTextCorrect;
-    int countCorrectAnswers;
+    private boolean editTextCorrect;
+    private int countCorrectAnswers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        //**Required for using custom Font**
+        // Set the font's path
+        String fontPath = "fonts/TNG_Title.ttf";
+        // Get the customFont TextView
+        TextView textView = (TextView) findViewById(R.id.quiz_title);
+        // Load the font as a TypeFace object
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), fontPath);
+        // Set the extra fancy font in the customFont TextView
+        textView.setTypeface(typeFace);
+
     }
 
     //Activity of clicked button
     public void clickedButton(View view) {
+
         answeredAll = true;
         editTextCorrect = false;
         countCorrectAnswers = 0;
@@ -43,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         RadioGroup radioGroup9 = (RadioGroup) findViewById(R.id.radio_group_9);
         RadioGroup radioGroup10 = (RadioGroup) findViewById(R.id.radio_group_10);
 
-        //Calls method ifAnswered() to check if are all radioGroups answered
+        //Calls method ifAnswered() to check if all are radioGroups answered
         ifAnswered(radioGroup1);
         ifAnswered(radioGroup2);
         ifAnswered(radioGroup4);
@@ -53,8 +67,11 @@ public class MainActivity extends AppCompatActivity {
         ifAnswered(radioGroup9);
         ifAnswered(radioGroup10);
 
-        //Calls method checkIfEditTextHasAnswer() to check if is EditText answered and if it is correct
+        //Calls method checkIfEditTextHasAnswer() to check if EditText is answered and if it is correct
         checkIfEditTextHasAnswer();
+
+        //Calls method checkCheckBoxes() to check if any checkbox is selected
+        checkCheckBoxes();
 
 
         //Show result
@@ -82,18 +99,69 @@ public class MainActivity extends AppCompatActivity {
             setRadioButtonBackgroundColor(radioGroup9, findViewById(R.id.radio_q9_1).getId());
             setRadioButtonBackgroundColor(radioGroup10, findViewById(R.id.radio_q10_2).getId());
 
-            //Colors background of EditText to green/red depending on answer
-            EditText editText = (EditText) findViewById(R.id.edit_text_answer);
-            if(editTextCorrect){
-                editText.setBackgroundColor(Color.GREEN);
-            }else{
-                editText.setBackgroundColor(Color.RED);
-            }
+            //Calls method colorEditTextBackground() to color background of EditText to green/red depending on answer
+            colorEditTextBackground();
 
-            Log.v("Bok!", "" + countCorrectAnswers);
+            //Calls method colorCheckBoxBackground() to color background of CheckBoxes
+            colorCheckBoxBackground();
 
+            //Calls method checkCorrectCheckBoxes()
+            checkCorrectCheckBoxes();
+
+            //Toast - quiz completed
+            Context context = getApplicationContext();
+            String text = getString(R.string.toast_part_one) + countCorrectAnswers + getString(R.string.toast_part_two);
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            //Unhides TextView result
+            TextView textViewResult = (TextView) findViewById(R.id.text_result);
+            textViewResult.setVisibility(View.VISIBLE);
+            String textResult = countCorrectAnswers + getString(R.string.text_final);
+            textViewResult.setText(textResult);
+
+            //Unhides GifImageView result
+            GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
+            gifImageView.setGifImageResource(getIdOfGif());
+            gifImageView.setVisibility(View.VISIBLE);
+
+        } else {
+            //Toast - not answered all questions
+            Context context = getApplicationContext();
+            String text = getString(R.string.toast_not_answered);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
 
+
+    }
+
+    /**
+     * Checks if selected CheckBoxes are correct
+     */
+    private void checkCorrectCheckBoxes() {
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox_1);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox_2);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox_3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkbox_4);
+        if (checkBox1.isChecked() && !checkBox2.isChecked() && !checkBox3.isChecked() && checkBox4.isChecked()) {
+            countCorrectAnswers++;
+        }
+    }
+
+    /**
+     * Checks if radio group has selected radio button
+     *
+     * @param radioGroup - current radio group in checking if selected any radio button
+     */
+    private void ifAnswered(RadioGroup radioGroup) {
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
+            answeredAll = false;
+        }
     }
 
     /**
@@ -103,39 +171,100 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.edit_text_answer);
         if (editText.getText().toString().equals("")) {
             answeredAll = false;
-        } else if (editText.getText().toString().equals("Vulcan")) {
+        } else if (editText.getText().toString().toUpperCase().equals(getString(R.string.text_vulcan))) {
             editTextCorrect = true;
             countCorrectAnswers++;
         }
     }
 
-
     /**
-     * Checks if radio group has selected radio button
+     * Checks if any checkbox is selected
      */
-    private void ifAnswered(RadioGroup radioGroup) {
-        if (radioGroup.getCheckedRadioButtonId() == -1) {
+    private void checkCheckBoxes() {
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox_1);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox_2);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox_3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkbox_4);
+        if (!checkBox1.isChecked() && !checkBox2.isChecked() && !checkBox3.isChecked() && !checkBox4.isChecked()) {
             answeredAll = false;
         }
     }
 
     /**
      * Sets background color for selected radio button and corect answer
+     *
+     * @param radioGroup       - current radio group of selected radio button
+     * @param radioBtnCorectId - id of correct radio button
      */
     private void setRadioButtonBackgroundColor(RadioGroup radioGroup, int radioBtnCorectId) {
         if (radioGroup.getCheckedRadioButtonId() != radioBtnCorectId) {
             RadioButton radioButtonChecked = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
             radioButtonChecked.setBackgroundColor(Color.RED);
-        } else{
+        } else {
             countCorrectAnswers++;
         }
         findViewById(radioBtnCorectId).setBackgroundColor(Color.GREEN);
-
     }
 
+    /**
+     * Colors background of EditText to green/red depending on answer
+     */
+    private void colorEditTextBackground() {
+        EditText editText = (EditText) findViewById(R.id.edit_text_answer);
+        if (editTextCorrect) {
+            editText.setBackgroundColor(Color.GREEN);
+        } else {
+            editText.setBackgroundColor(Color.RED);
+        }
+    }
 
     /**
-     * Fragment's state saved.
+     * Colors background of CheckBoxes to green/red depending on answers
+     */
+    private void colorCheckBoxBackground() {
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkbox_1);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkbox_2);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkbox_3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkbox_4);
+
+        if (checkBox1.isChecked()) {
+            checkBox1.setBackgroundColor(Color.GREEN);
+        }
+        if (checkBox2.isChecked()) {
+            checkBox2.setBackgroundColor(Color.RED);
+        }
+        if (checkBox3.isChecked()) {
+            checkBox3.setBackgroundColor(Color.RED);
+        }
+        if (checkBox4.isChecked()) {
+            checkBox4.setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    /**
+     * Gets id of the drawable resource gif depending on number of correct answers
+     *
+     * @return id of the drawable resource gif
+     */
+    private int getIdOfGif() {
+
+        int idOfGif = 0;
+
+        if (countCorrectAnswers <= 3) {
+            idOfGif = R.drawable.kirk_pain;
+        } else if (countCorrectAnswers <= 6) {
+            idOfGif = R.drawable.krik_disapointed;
+        } else if (countCorrectAnswers <= 9) {
+            idOfGif = R.drawable.kirk_statisfied;
+        } else if (countCorrectAnswers == 10) {
+            idOfGif = R.drawable.kirk_impressed;
+        }
+
+        return idOfGif;
+    }
+
+    /**
+     * Fragment's state saved before clicking button.
      */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -182,12 +311,10 @@ public class MainActivity extends AppCompatActivity {
 
         final RadioGroup radioGroup10 = (RadioGroup) findViewById(R.id.radio_group_10);
         outState.putInt("radioGroup10", radioGroup10.getCheckedRadioButtonId());
-
-
     }
 
     /**
-     * Fragment's state restore.
+     * Fragment's state restore before clicking button.
      */
     @Override
     protected void onRestoreInstanceState(Bundle savedState) {
@@ -234,8 +361,6 @@ public class MainActivity extends AppCompatActivity {
 
         final RadioGroup radioGroup10 = (RadioGroup) findViewById(R.id.radio_group_10);
         radioGroup10.check(savedState.getInt("radioGroup10"));
-
     }
-
 
 }
